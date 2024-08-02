9e0b6205da5d4ff07474248bee6072882f45cbf9
irchamzah <71950891+irchamzah@users.noreply.github.com>
2024-08-02 15:28:55 +0700

    Create Hero Section & Collection Section

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const cart = [];
-
-  document.querySelectorAll(".product").forEach((product) => {
-    product.addEventListener("click", () => {
-      const productName = product.querySelector("h2").innerText;
-      cart.push(productName);
-      alert(`${productName} added to cart!`);
-    });
-  });
+document.getElementById("menu-toggle").addEventListener("click", function () {
+  var menu = document.getElementById("menu");
+  var icon = this;
+  if (menu.style.display === "block") {
+    menu.style.display = "none"; /* Sembunyikan menu jika sudah ditampilkan */
+    icon.classList.remove("fa-xmark"); /* Kembalikan ikon ke fa-bars */
+    icon.classList.add("fa-bars");
+  } else {
+    menu.style.display = "block"; /* Tampilkan menu jika tidak ditampilkan */
+    icon.classList.remove("fa-bars"); /* Ganti ikon menjadi fa-xmark */
+    icon.classList.add("fa-xmark");
+  }
 });
